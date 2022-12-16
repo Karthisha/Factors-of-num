522095659a39f9e6288d518062b3cb0220974482
Karthisha <116002042+Karthisha@users.noreply.github.com>
2022-12-16 18:20:15 +0530

    Add files via upload

---
+++ Factors.py
@@ -0,0 +1,16 @@
+num = int(input("Enter a number: "))
+
+# To take input from the user
+# prime numbers are greater than 1
+if num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(i,"is a factor of",num)
+   else:
+       print(num,"is a prime number")
+       
+# if input number is less than
+# or equal to 1, it is not prime
+else:
+   print(num,"is not a prime number")
